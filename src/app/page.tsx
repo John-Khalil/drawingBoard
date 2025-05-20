4ee233d105209f48ddda6bf8c852cc8fab82df93
John Khalil <104776571+John-Khalil@users.noreply.github.com>
2025-05-21 01:24:21 +0300

    shadCN added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Button variant="outline">Login</Button>
       <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+        Login
+      </h1>
+      <Button variant="outline">Login</Button>
     </>
   );
 }

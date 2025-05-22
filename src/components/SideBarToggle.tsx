23dc358ab3212417babe9464edf59a80941c35ab
John-Khalil <johnadelkhalil@gmail.com>
2025-05-22 05:51:04 +0300

    wip

--- src/components/SideBarToggle.tsx
+++ src/components/SideBarToggle.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Menu } from 'lucide-react';
+import { Button } from './ui/button';
 
 const SideBarToggle = () => {
   return (
-    <Menu/>
+    <Button variant="ghost" className='absolute top-2.5 left-5 z-50'>
+      <Menu size={32}/>
+    </Button>
   )
 }
 

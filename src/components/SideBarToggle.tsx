6a683ca9207cd40619e5faf8e61bbb36d2069b0e
John-Khalil <johnadelkhalil@gmail.com>
2025-07-01 08:24:23 +0300

    wip

--- src/components/SideBarToggle.tsx
+++ src/components/SideBarToggle.tsx
@@ -4,13 +4,19 @@ import { Button } from './ui/button';
 
 const SideBarToggle = () => {
   return (
-    <button>
-      <Menu size={32}/>
+    <div className='flex items-center'>
+      {/* <button>
+        <Menu size={32}/>
 
-    </button>
-    // <Button variant="ghost" className='absolute top-2.5 left-5 z-50 p-0 m-0 '>
-      
-    // </Button>
+      </button> */}
+      <Button variant="ghost" className=' z-50 p-0 m-2 ' size="icon" asChild
+        onClick={()=>{
+          console.log("test")
+        }}
+      >
+        <Menu size={64}/>
+      </Button>
+    </div>
   )
 }
 

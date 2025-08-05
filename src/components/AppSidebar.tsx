b3af471214a251d7cf13f3eec1d0158bdc37752e
John-Khalil <johnadelkhalil@gmail.com>
2025-08-05 04:58:04 +0300

    wip: shadCn sidebar setup

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,11 @@
+//? https://ui.shadcn.com/docs/components/sidebar#your-first-sidebar
+
+import { Sidebar } from "./ui/sidebar";
+
+export function AppSidebar(){
+  return(
+    <>
+     <Sidebar variant="sidebar"/>
+    </>
+  )
+}
--- src/components/SideBarToggle.tsx
+++ src/components/SideBarToggle.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { Menu } from 'lucide-react';
 import { Button } from './ui/button';
